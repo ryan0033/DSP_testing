b90101c8b3dbd618e63effdeb05d688b573eeba6
DTxLab <digitalhealth804lab@gmail.com>
2023-11-28 21:01:42 +0800

    Project Initialize

--- app/src/main/java/com/example/x/MainActivity.java
+++ app/src/main/java/com/example/x/MainActivity.java
@@ -1,10 +1,11 @@
-package com.example.x; //注意
+package com.example.x; // 注意：這裡應該是你的應用程式包名稱
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -22,7 +23,6 @@ import be.tarsos.dsp.mfcc.MFCC;
 public class MainActivity extends AppCompatActivity {
 
     private static final int READ_REQUEST_CODE = 42;
-    // private MFCCCalculator mfccCalculator;
     private TextView mfccTextView;
 
     private String getPathFromUri(Uri uri) {
@@ -39,6 +39,48 @@ public class MainActivity extends AppCompatActivity {
 
         return null;
     }
+
+    private String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                    if (nameIndex != -1) {
+                        result = cursor.getString(nameIndex);
+                    } else {
+                        // 若 DISPLAY_NAME 欄位不存在，嘗試獲取其他相關欄位
+                        // 例如 MediaStore.Audio.Media.TITLE 或 MediaStore.Audio.Media._ID
+                        // 如果有其他適合的欄位，請替換它
+                        int titleIndex = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+                        if (titleIndex != -1) {
+                            result = cursor.getString(titleIndex);
+                        } else {
+                            int idIndex = cursor.getColumnIndex(MediaStore.Audio.Media._ID);
+                            if (idIndex != -1) {
+                                result = cursor.getString(idIndex);
+                            }
+                        }
+                    }
+                }
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+        }
+        if (result == null) {
+            // 如果仍然找不到適合的名稱，使用檔案路徑的最後一部分作為名稱
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,26 +109,29 @@ public class MainActivity extends AppCompatActivity {
         someActivityResultLauncher.launch(intent);
     }
 
-        ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                result -> {
-                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-                        // 獲取選擇的檔案路徑
-                        String filePath = getPathFromUri(result.getData().getData());
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    // 獲取選擇的檔案路徑
+                    String filePath = getPathFromUri(result.getData().getData());
 
-                        if (filePath != null) {
-                            // 計算 MFCC 並更新 TextView
-                            float[] mfccValues = calculateMFCC(filePath);
-                            updateMFCCTextView(mfccValues);
+                    if (filePath != null) {
+                        // 獲取選擇的檔案名稱
+                        String fileName = getFileName(result.getData().getData());
 
-                            // 在這裡添加上傳成功的處理代碼
-                            showUploadSuccessMessage();
-                        }
-                    } else {
-                        // 處理取消選擇或其他情況
-                        Toast.makeText(this, "選擇取消或發生錯誤", Toast.LENGTH_SHORT).show();
+                        // 計算 MFCC 並更新 TextView
+                        float[] mfccValues = calculateMFCC(filePath);
+                        updateMFCCTextView(mfccValues, fileName);
+
+                        // 在這裡添加上傳成功的處理代碼
+                        showUploadSuccessMessage();
                     }
-                });
+                } else {
+                    // 處理取消選擇或其他情況
+                    Toast.makeText(this, "選擇取消或發生錯誤", Toast.LENGTH_SHORT).show();
+                }
+            });
 
     private float[] calculateMFCC(String filePath) {
         try {
@@ -108,8 +153,8 @@ public class MainActivity extends AppCompatActivity {
         return new float[0];
     }
 
-    private void updateMFCCTextView(float[] mfccValues) {
-        StringBuilder sb = new StringBuilder("MFCC Values:\n");
+    private void updateMFCCTextView(float[] mfccValues, String fileName) {
+        StringBuilder sb = new StringBuilder("MFCC Values for file: " + fileName + "\n");
         for (float value : mfccValues) {
             sb.append(value).append("\n");
         }
